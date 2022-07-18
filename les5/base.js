1a35fc0d3c5072990c025389f39f1b343f118a99
Vladimir Belich <belichvladimir@mail.ru>
2022-07-18 13:24:00 +0300

    fix les5

--- les5/base.js
+++ les5/base.js
@@ -1,22 +1,21 @@
 (() => {
-    let playing = true,
-      activeHole = 1;
-  
-    const stop = () => playing = true,
-      getHole = index => document.getElementById(`hole${index}`),
-      deactivateHole = index =>
-        getHole( index ).className = 'hole',
-      activateHole = index =>
-        getHole( index ).className = 'hole hole_has-mole',
-      next = () => setTimeout(() => {
-        if ( !playing ) {
+  let playing = true,
+    activeHole = 1;
+
+  const stop = () => (playing = true),
+    getHole = (index) => document.getElementById(`hole${index}`),
+    deactivateHole = (index) => (getHole(index).className = "hole"),
+    activateHole = (index) => (getHole(index).className = "hole hole_has-mole"),
+    next = () =>
+      setTimeout(() => {
+        if (!playing) {
           return;
         }
-        deactivateHole( activeHole );
-        activeHole = Math.floor( 1 + Math.random() * 9 );
-        activateHole( activeHole );
+        deactivateHole(activeHole);
+        activeHole = Math.floor(1 + Math.random() * 9);
+        activateHole(activeHole);
         next();
-      }, 800 );
-  
-    next();
-  })();
+      }, 800);
+
+  next();
+})();
--- les5/task.js
+++ les5/task.js
@@ -1,28 +1,31 @@
-getHole = index => document.getElementById(`hole${index}`)
-const n = document.getElementsByClassName('hole').length
+getHole = (index) => document.getElementById(`hole${index}`);
+const n = document.getElementsByClassName("hole").length;
 // console.log(document.querySelectorAll('.hole-game div')[0].id)
-for (i=1;i<n;i++) (function(i){
+for (i = 1; i < n; i++)
+  (function (i) {
     getHole(i).onclick = () => {
-    checkClass(getHole(i).className)
-    return false
-}})(i)
-function checkWinLose ( text) {
-        alert(text)
-        document.getElementById(`dead`).textContent=0
-        document.getElementById(`lost`).textContent=0
+      checkClass(getHole(i).className);
+      return false;
+    };
+  })(i);
+function checkWinLose(text) {
+  alert(text);
+  document.getElementById(`dead`).textContent = 0;
+  document.getElementById(`lost`).textContent = 0;
 }
-function checkClass (className){
-    console.log(className)
-    if (className == 'hole hole_has-mole') {
-        document.getElementById(`dead`).textContent = Number(document.getElementById(`dead`).textContent)+1;
-        if (document.getElementById('dead').textContent>=5) {
-        checkWinLose ('Выиграли');
-        }
+function checkClass(className) {
+  console.log(className);
+  if (className == "hole hole_has-mole") {
+    document.getElementById(`dead`).textContent =
+      Number(document.getElementById(`dead`).textContent) + 1;
+    if (document.getElementById("dead").textContent >= 5) {
+      checkWinLose("Выиграли");
     }
-    else {
-        document.getElementById(`lost`).textContent = Number(document.getElementById(`lost`).textContent)+1;
-        if (document.getElementById('lost').textContent>=5) {
-        checkWinLose ('Проиграли');
-        }
+  } else {
+    document.getElementById(`lost`).textContent =
+      Number(document.getElementById(`lost`).textContent) + 1;
+    if (document.getElementById("lost").textContent >= 5) {
+      checkWinLose("Проиграли");
     }
+  }
 }
