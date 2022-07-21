786d7c42383da2618c44721511574bf15c9ecbd2
Vladimir Belich <belichvladimir@mail.ru>
2022-07-21 17:16:48 +0300

    fix les7/1/task.js

--- les7/1/task.js
+++ les7/1/task.js
@@ -1,18 +1,17 @@
 const item = document.querySelector('.dropdown')
 const items = document.querySelectorAll('.dropdown__item')
-const eventClickMenu = function() {
-    if (document.querySelector('.dropdown__list').className=='dropdown__list') {
+console.log(items)
+item.onclick = () => {
+       if (document.querySelector('.dropdown__list').className=='dropdown__list') {
         document.querySelector('.dropdown__list').className = 'dropdown__list dropdown__list_active'}
     else {
         document.querySelector('.dropdown__list').className = 'dropdown__list' 
-    }
+    } 
+    return false
 }
-const eventClickMenuItem = () => {
-    document.querySelector('.dropdown__value').innerText = event.target.textContext
-}
-for (i=0, len=items.length; i<len; i++) (function (i){
-    items[i].addEventListener('click',eventClickMenuItem)
-})(i)
-item.addEventListener('click',eventClickMenu)
+for (i=0; i<items.length; i++) {
+    items[i].onclick=(e)=>{
+        console.log(document.querySelector('.dropdown__value').textContent = e.srcElement.textContent)
+    }}
 
 
