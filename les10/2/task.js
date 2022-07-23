1d47843b2dc23b323f95317a1ac7b6167f5ab027
Vladimir Belich <belichvladimir@mail.ru>
2022-07-23 18:58:41 +0300

    add les10/2

---
+++ les10/2/task.js
@@ -0,0 +1,15 @@
+const tasks = document.getElementById('tasks__list')
+const input = document.getElementById('task__input')
+const btn = document.querySelector('.tasks__add')
+window.addEventListener('keypress', (e) => {
+    if (e.key=='Enter'){
+    tasks.insertAdjacentHTML('beforeEnd','<div class="task"><div class="task__title">'+input.value+'</div><a href="#" class="task__remove">&times;</a></div>')
+    const removeList = document.querySelectorAll('.task__remove')
+        for (let index = 0; index < removeList.length; index++) {
+        removeList[index].addEventListener('click', (e) => {
+            e.target.removeEventListener('click',e.target.parentElement.remove())
+        });
+    }
+    }
+    return false
+})
