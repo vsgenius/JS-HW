f3cf4454d86aece06f234b02782add3768f17a19
Vladimir Belich <belichvladimir@mail.ru>
2022-07-24 19:07:48 +0300

    fix les9/2

--- les9/2/task.js
+++ les9/2/task.js
@@ -1,6 +1,18 @@
 const interests = document.querySelectorAll('.interest__check')
 for (i=0; i<interests.length;i++) {
+// console.log(interests[i])
 interests[i].addEventListener('change',(e)=>{
-    console.log(e)
+    if (e.target.parentElement.nextElementSibling!=null) {
+        // console.log(e.target.closest('.interest').children[1].querySelectorAll('.interest'))
+        const listCheckbox = e.target.closest('.interest').children[1].querySelectorAll('input')
+        for (let index = 0; index < listCheckbox.length; index++) {
+            if (e.target.checked==true) {
+            listCheckbox[index].checked=true}
+            else {
+                listCheckbox[index].checked=false
+            }
+        }
+    }
+    // nextElementSibling
 })
 }
