d3a8d6c1e5f3b189175b141c587b399a1d9dc61b
Vladimir Belich <belichvladimir@mail.ru>
2022-07-31 12:31:41 +0300

    fix les10

--- les10/1/task.js
+++ les10/1/task.js
@@ -3,26 +3,26 @@ const inc = document.querySelectorAll('.product__quantity-control_inc')
 const addcart = document.querySelectorAll('.product__add')
 for (let index = 0; index < dec.length; index++) {
     dec[index].addEventListener('click', (e) => {
-        --e.srcElement.parentElement.querySelector('.product__quantity-value').innerText
+        --e.target.parentElement.querySelector('.product__quantity-value').innerText
     }) 
 }
 for (let index = 0; index < inc.length; index++) {
     inc[index].addEventListener('click', (e) => {
-        ++e.srcElement.parentElement.querySelector('.product__quantity-value').innerText
+        ++e.target.parentElement.querySelector('.product__quantity-value').innerText
     }) 
 }
 for (let index = 0; index < addcart.length; index++) {
     addcart[index].addEventListener('click', (e) => {
         const productCart = document.querySelector('.cart__products')
-        const count =e.srcElement.parentElement.querySelector('.product__quantity-value').innerText
+        const count =e.target.parentElement.querySelector('.product__quantity-value').innerText
         const id = e.path[3].getAttribute('data-id')
         const img = e.path[3].querySelector('.product__image').getAttribute('src')
         const cart = document.querySelectorAll('.cart__product')
-        console.log(cart.length)
         if (document.querySelectorAll('.cart__product').length==0) {
             productCart.insertAdjacentHTML('beforeEnd','<div class="cart__product" data-id="'+id+'"><img class="cart__product-image" src="'+img+'"><div class="cart__product-count">'+count+'</div></div>')
         }
         else {
+            // console.log(Array.from(cart).indexOf(id,0))
             for (let index = 0; index < cart.length; index++) {
                 if (id==cart[index].getAttribute('data-id')) {
                     cart[index].querySelector('.cart__product-count').textContent=Number(cart[index].querySelector('.cart__product-count').textContent)+Number(count)
--- les10/3/task.js
+++ les10/3/task.js
@@ -1,12 +1,18 @@
 const tooltips = document.querySelectorAll('.has-tooltip')
 for (let index = 0; index < tooltips.length; index++) {
   tooltips[index].onclick = (e)=>{
-    if (document.querySelector('.tooltip_active') != null) {
-      document.querySelector('.tooltip_active').delete()
+    const active = document.querySelector('.tooltip_active')
+    if (active != null) {
+      if (e.target.nextSibling==active) {
+        active.remove()
+      }
+      else {
+      active.remove()
       e.target.insertAdjacentHTML('afterEnd','<div class="tooltip tooltip_active" style="left: '
       +e.target.getBoundingClientRect().left+'px; top: '
       +e.target.getBoundingClientRect().bottom+'px">'
       +e.target.getAttribute('title')+'</div>')
+      }
     }
     else {
       e.target.insertAdjacentHTML('afterEnd','<div class="tooltip tooltip_active" style="left: '
